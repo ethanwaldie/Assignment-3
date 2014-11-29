a230f9128597d60f61c30be2788eb064a33741d9
junka96 <andrej.janda@yahoo.ca>
2014-11-28 20:14:15 -0500

    try this

--- calendar.py
+++ calendar.py
@@ -1,9 +1,16 @@
 
 
 
+course_desc_page = "http://www.cs.toronto.edu/~guerzhoy/180/assignments/a3/csc_short.htm"
 
+def get_individual_courses(course_desc_page):
+    import urllib.request
+    f = urllib.request.urlopen(course_desc_page)
+    page = f.read().decode("utf-8")
+    f.close()
+    print(page)    
 
-def get_individual_courses(
-
+if __name__ == '__main__':
+    get_individual_courses(course_desc_page)
 
 
