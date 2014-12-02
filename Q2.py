f527be9fd1f1dfcdf0df974f7c0d293ad0d81e02
ethanwaldie1996 <ethan.waldo2@gmail.com>
2014-12-02 17:16:43 -0500

    Q2 finised

--- Q2.py
+++ Q2.py
@@ -1,37 +1,59 @@
-courses = { "A": [ ["B"], ["C"] ],
+paths = { "A": [ ["B","C"]],
             "B": [ ["D"], ["E"] ],
             "D": [ ["F"], ["G"] ],
-            "C": [ ["H"] ] }
+            "C": [ ["H"], ["X"] ] }
 
 def expand_one_course(req, path):
-    print(req)
+    #print(req)
     
     prereq = []
     
     for i in range(len(req)):
-        print(req[i])
-        print(req[i][-1])
         if req[i][-1] not in path.keys():
             prereq.append(req[i])
-            return prereq
         else:
             for x in range(len(path[req[i][-1]])):
-                print(path[req[i][-1]])
                 prereq.append(req[i] + path[req[i][-1]][x])
-                print(prereq)
     
     return prereq
     
 def expand_all_courses(course, path):
     Base = []
+
+    course = expand_one_course(course, path)
     
     for option in course:
-        print("option", option[-1])
         if option[-1] not in path.keys():
             Base.append(True)
         else:
             Base.append(False)
+    
     if False not in Base:
         return course
         
-    return expand_all_courses(expand_one_course(course, path), path)
+    return expand_all_courses(course, path)
+
+
+def get_all_paths_to_course(course, path):
+    
+    options = []
+    for prereq in path[course]:
+        for item in prereq:
+            tmp = []
+            if item in path.keys():
+                tmp = (expand_all_courses([[item]], path))
+                print(tmp)
+            
+            if len(options) == 0:
+                options = tmp
+            else:
+
+                tmpath = options[:]
+                options = []
+                for x in range(len(tmp)):
+                    for y in range(len(tmpath)):
+                        options.append(tmpath[y] + tmp[x])
+                        
+            print ("options", options)
+    return options
+                    
--- calendar.py
+++ calendar.py
@@ -144,6 +144,60 @@ def build_prerequisite_dict(course_desc_page):
     
     return prerequisite_dict
 
+def expand_one_course(req, path):
+    #print(req)
+    
+    prereq = []
+    
+    for i in range(len(req)):
+        if req[i][-1] not in path.keys():
+            prereq.append(req[i])
+        else:
+            for x in range(len(path[req[i][-1]])):
+                prereq.append(req[i] + path[req[i][-1]][x])
+    
+    return prereq
+    
+def expand_all_courses(course, path):
+    Base = []
+
+    course = expand_one_course(course, path)
+    
+    for option in course:
+        if option[-1] not in path.keys():
+            Base.append(True)
+        else:
+            Base.append(False)
+    
+    if False not in Base:
+        return course
+        
+    return expand_all_courses(course, path)
+
+
+def get_all_paths_to_course(course, path):
+    
+    options = []
+    for prereq in path[course]:
+        for item in prereq:
+            tmp = []
+            if item in path.keys():
+                tmp = (expand_all_courses([[item]], path))
+                print(tmp)
+            
+            if len(options) == 0:
+                options = tmp
+            else:
+
+                tmpath = options[:]
+                options = []
+                for x in range(len(tmp)):
+                    for y in range(len(tmpath)):
+                        options.append(tmpath[y] + tmp[x])
+                        
+            print ("options", options)
+    return options
+                    
 
 if __name__ == '__main__':
     
@@ -151,4 +205,6 @@ if __name__ == '__main__':
 
     
     testing = build_prerequisite_dict(course_desc_page)
+    
+    testing = get_all_paths_to_course(CSC, testing)
 
